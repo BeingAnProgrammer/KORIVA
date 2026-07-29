98dadfb26ed10bb8c9e39b61901571d0ca9d6810
Nitesh <nitesh20rv@gmail.com>
2026-07-29 23:16:18 +0530

    feat: update environment files with Supabase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://uxnnobbclgabusrxfszm.supabase.co',
+  supabaseAnonKey: 'sb_publishable_R310_aKF8hnY5OwHsitSWw_C0yPWz5e',
   apiUrl: ''
 };
